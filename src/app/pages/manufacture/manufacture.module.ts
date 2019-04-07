acb09706f627ee4680a7968cef3aca7095c8f651
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-07 15:01:14 +0530

    add table for manufacture

--- src/app/pages/manufacture/manufacture.module.ts
+++ src/app/pages/manufacture/manufacture.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ManufactureRoutingModule } from './manufacture-routing.module';
 import { ManufactureComponent } from './manufacture.component';
 
+//for import the smart table
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 @NgModule({
   declarations: [ManufactureComponent],
   imports: [
     CommonModule,
-    ManufactureRoutingModule
+    ManufactureRoutingModule,
+    Ng2SmartTableModule
   ]
 })
 export class ManufactureModule { }
