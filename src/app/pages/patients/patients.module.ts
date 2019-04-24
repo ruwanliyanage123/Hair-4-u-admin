0f19b7f78fc70208a25f0cf2eb39350e11a87c0a
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-24 18:01:01 +0530

    add waiting component

--- src/app/pages/patients/patients.module.ts
+++ src/app/pages/patients/patients.module.ts
@@ -9,9 +9,10 @@ import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { FinishedComponent } from './finished/finished.component';
 import { WigsComponent } from './wigs/wigs.component';
 import { CompletedComponent } from './wigs/completed/completed.component';
+import { WaitingComponent } from './wigs/waiting/waiting.component';
 
 @NgModule({
-  declarations: [PatientsComponent, FinishedComponent, WigsComponent, CompletedComponent],
+  declarations: [PatientsComponent, FinishedComponent, WigsComponent, CompletedComponent, WaitingComponent],
   imports: [CommonModule, PatientsRoutingModule, Ng2SmartTableModule]
 })
 export class PatientsModule {}
