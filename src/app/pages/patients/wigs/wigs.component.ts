cdcc488d8d413a244dd53df161c5467fb6cfe281
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-04 16:20:40 +0530

    make html for display the cards

--- src/app/pages/patients/wigs/wigs.component.ts
+++ src/app/pages/patients/wigs/wigs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Finished } from '../finished/finished.model';
+import { FinishedService } from '../finished/finished.service';
 
 @Component({
   selector: 'ngx-wigs',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wigs.component.scss']
 })
 export class WigsComponent implements OnInit {
+  list: Finished[] = [];
 
-  constructor() { }
+  constructor(private service: FinishedService) {}
 
   ngOnInit() {
+    this.service.getPatients().subscribe(actionArray => {
+      let patients_data = actionArray.payload.get('data');
+      if (patients_data) {
+        this.list = patients_data;
+      }
+    });
   }
-
 }
--- src/app/pages/patients/wigs/wigs.module.ts
+++ src/app/pages/patients/wigs/wigs.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WaitingModule } from './waiting/waiting.module';
-import { WaitingComponent } from './waiting/waiting.component';
+
 import { WigsComponent } from './wigs.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CompletedComponent } from './completed/completed.component';
 import { WigsRoutingModule } from './wigs-routing.module';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [WigsComponent, CompletedComponent],
@@ -15,7 +21,12 @@ import { WigsRoutingModule } from './wigs-routing.module';
     WaitingModule,
     MatDialogModule,
     MatFormFieldModule,
-    WigsRoutingModule
+    WigsRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatButtonModule
   ]
 })
 export class WigsModule {}
