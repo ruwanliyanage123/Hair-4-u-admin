89f5ffe746543aaa763f1941619792a3e74e18f2
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-06-02 22:23:06 +0530

    make changes in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,57 +1,43 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import {
-  NbAuthComponent,
-  NbLoginComponent,
-  NbLogoutComponent,
-  NbRegisterComponent,
-  NbRequestPasswordComponent,
-  NbResetPasswordComponent,
-} from '@nebular/auth';
+
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
   {
-    path: 'auth',
-    component: NbAuthComponent,
-    children: [
-      {
-        path: '',
-        component: NbLoginComponent,
-      },
-      {
-        path: 'login',
-        component: NbLoginComponent,
-      },
-      {
-        path: 'register',
-        component: NbRegisterComponent,
-      },
-      {
-        path: 'logout',
-        component: NbLogoutComponent,
-      },
-      {
-        path: 'request-password',
-        component: NbRequestPasswordComponent,
-      },
-      {
-        path: 'reset-password',
-        component: NbResetPasswordComponent,
-      },
-    ],
+    path: 'sign-in',
+    component: SignInComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'register-user',
+    component: SignUpComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
+    //canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'verify-email-address',
+    component: VerifyEmailComponent
+    //canActivate: [SecureInnerPagesGuard]
   },
   { path: '', redirectTo: 'Pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'Pages' },
+  { path: '**', redirectTo: 'Pages' }
 ];
 
 const config: ExtraOptions = {
-  useHash: true,
+  useHash: true
 };
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, config)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
