1aa2d13fc375cb7f38d443de460bbd36f195566d
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-06-02 21:38:10 +0530

    add verifi-email component

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.css']
 })
 export class VerifyEmailComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
