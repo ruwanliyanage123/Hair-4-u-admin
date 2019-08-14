4348845b3ea2079304d79a9558f03a6f0a2a38fb
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-08-14 09:41:41 +0530

    add email component

--- src/app/pages/patients/patients.module.ts
+++ src/app/pages/patients/patients.module.ts
@@ -20,9 +20,10 @@ import {
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
-  declarations: [PatientsComponent, FinishedComponent, ProfileComponent],
+  declarations: [PatientsComponent, FinishedComponent, ProfileComponent, EmailComponent],
   imports: [
     CommonModule,
     PatientsRoutingModule,
